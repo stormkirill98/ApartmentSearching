3e0fe71c15d93ebd9c29dc24bf99be7fc828c3f0
stormkirill98 <stormkirill98@gmail.com>
2019-12-28 13:43:37 +0300

    setup run autoreload, but this is now working

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,33 +3,36 @@ package com.group
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.CORS
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 
-fun Application.main() {
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
     // This adds Date and Server headers to each response, and allows custom additional headers
     install(DefaultHeaders)
     // This uses use the logger to log every call (request/response)
     install(CallLogging)
 
-/*    install(CORS) {
+    install(CORS) {
         method(HttpMethod.Options)
         method(HttpMethod.Put)
         method(HttpMethod.Delete)
         method(HttpMethod.Patch)
         header(HttpHeaders.Authorization)
-        header("MyCustomHeader")
         allowCredentials = true
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
-    }*/
+    }
 
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            call.respondText("HELLO WORLD123!", contentType = ContentType.Text.Plain)
         }
     }
 }
