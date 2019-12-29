eeb63e912cec443a38b701a9ad5c4076b32108f7
stormkirill98 <stormkirill98@gmail.com>
2019-12-30 01:51:09 +0300

    create test keyboard

--- src/main/kotlin/services/vk/VkApi.kt
+++ src/main/kotlin/services/vk/VkApi.kt
@@ -4,6 +4,7 @@ import com.group.services.getProperty
 import com.vk.api.sdk.client.VkApiClient
 import com.vk.api.sdk.client.actors.GroupActor
 import com.vk.api.sdk.httpclient.HttpTransportClient
+import com.vk.api.sdk.objects.messages.*
 import org.slf4j.LoggerFactory
 import kotlin.random.Random
 
@@ -18,11 +19,33 @@ object VkApi {
     private val actor = GroupActor(groupId, accessKey)
 
     fun sendMsg(peerId: Int, msg: String) {
+        val keyboard = listOf(
+            listOf(
+                KeyboardButton().apply {
+                    color = KeyboardButtonColor.NEGATIVE
+                    action = KeyboardButtonAction().apply {
+                        type = KeyboardButtonActionType.TEXT
+                        payload = "{\"command\":\"start\"}"
+                        label = "123dsa"
+                    }
+
+                }
+            )
+        )
+
         vkApi.messages()
             .send(actor)
             .peerId(peerId)
             .randomId(Random.nextInt())
             .message(msg)
+            .keyboard(
+                Keyboard()
+                    .setInline(false)
+                    .setOneTime(true)
+                    .setButtons(
+                        keyboard
+                    )
+            )
             .execute()
     }
 }
