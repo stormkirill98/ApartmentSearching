b2909f228435879e95e20db58370e4a91243bce4
stormkirill98 <stormkirill98@gmail.com>
2020-01-29 20:45:43 +0300

    wrong connect to database from localhost

--- src/main/kotlin/Bootstrapper.kt
+++ src/main/kotlin/Bootstrapper.kt
@@ -1,19 +1,35 @@
 package com.group
 
+import com.group.services.vk.VkApi
 import org.jetbrains.exposed.sql.Database
+import org.slf4j.LoggerFactory
+import java.net.InetAddress
 import javax.servlet.ServletContextEvent
 import javax.servlet.ServletContextListener
 
 @Suppress("unused")
 class Bootstrapper : ServletContextListener {
-    override fun contextInitialized(sce: ServletContextEvent?) {
-        Database.connect(
-            "jdbc:postgresql:///postgres" +
-                    "?cloudSqlInstance=apartment-searching:europe-west3:myinstance" +
-                    "&socketFactory=com.google.cloud.sql.postgres.SocketFactory" +
-                    "&user=postgres&password=admin",
-            driver = "org.postgresql.Driver"
-        )
+    private val logger = LoggerFactory.getLogger(Bootstrapper::class.java)
+
+    override fun contextInitialized(sce: ServletContextEvent) {
+        // maybe is KOSTYL. Add your host to white list!
+        if (InetAddress.getLoopbackAddress().hostAddress == "127.0.0.1") {
+            Database.connect(
+                "jdbc:postgresql://35.242.227.75:5432/postgres",
+                driver = "org.postgresql.Driver",
+                user = "postgres",
+                password = "admin"
+            )
+        } else {
+            Database.connect(
+                "jdbc:postgresql:///postgres" +
+                        "?cloudSqlInstance=apartment-searching:europe-west3:myinstance" +
+                        "&socketFactory=com.google.cloud.sql.postgres.SocketFactory",
+                driver = "org.postgresql.Driver",
+                user = "postgres",
+                password = "admin"
+            )
+        }
     }
 
     override fun contextDestroyed(sce: ServletContextEvent?) {}
